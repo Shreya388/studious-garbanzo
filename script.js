f3f67ab3db82a85d548dcaf3ed8557dddb473869
Shreya <xjshreya37@gmail.com>
2021-05-05 21:59:37 +0530

    change bgcolor script

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function changeSentence(){
     document.getElementById("demo").innerHTML = "what up";
     alert("it worked :)");
 }
+
+function makeBlue(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function makeBlack(){
+    document.body.style.backgroundColor = "black";
+}
+
+$(".black").hide();
+    $(".blue").hide();
+
+$(".changeBg").click(function(){
+    $(".black").show();
+    $(".blue").show();
+  });
+
